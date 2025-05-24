d82c61cf0716d464525c0465480171e8e3e3bb63
liuyisi123 <46984336+liuyisi123@users.noreply.github.com>
2025-05-24 18:15:48 +0800

    Update attention.py

--- models/attention.py
+++ models/attention.py
@@ -16,17 +16,26 @@ class HybridAttentionModule(nn.Module):
         """
         super(HybridAttentionModule, self).__init__()
         self.channels = channels
-        self.num_groups = num_groups
+        self.num_groups = min(num_groups, channels)  # Ensure num_groups doesn't exceed channels
         
-        # Group normalization for spatial attention
-        self.group_norm = nn.GroupNorm(min(num_groups, channels), channels)
+        # Ensure channels can be divided by num_groups
+        if channels % self.num_groups != 0:
+            self.num_groups = channels // (channels // self.num_groups)
+        
+        self.channels_per_group = channels // self.num_groups
+        
+        # Group normalization for spatial attention - separate GroupNorm for each group
+        self.group_norms = nn.ModuleList([
+            nn.GroupNorm(1, self.channels_per_group // 2) 
+            for _ in range(self.num_groups)
+        ])
         
         # Channel attention parameters
         self.channel_attention = nn.ModuleList([
             nn.Sequential(
                 nn.Linear(1, 1),
                 nn.Sigmoid()
-            ) for _ in range(num_groups)
+            ) for _ in range(self.num_groups)
         ])
         
         # Spatial attention parameters
@@ -34,7 +43,7 @@ class HybridAttentionModule(nn.Module):
             nn.Sequential(
                 nn.Linear(1, 1),
                 nn.Sigmoid()
-            ) for _ in range(num_groups)
+            ) for _ in range(self.num_groups)
         ])
         
     def forward(self, x):
@@ -51,25 +60,25 @@ class HybridAttentionModule(nn.Module):
         batch_size, C, L = x.shape
         
         # Split features into groups
-        x_groups = x.view(batch_size, self.num_groups, C // self.num_groups, L)
+        x_groups = x.view(batch_size, self.num_groups, self.channels_per_group, L)
         
         # Process each group
         output_groups = []
         for g in range(self.num_groups):
             # Get current group
-            x_g = x_groups[:, g, :, :]  # [batch_size, C//G, L]
+            x_g = x_groups[:, g, :, :]  # [batch_size, channels_per_group, L]
             
             # Split into two branches for channel and spatial attention
-            c_per_group = x_g.size(1)
-            x_g1, x_g2 = torch.split(x_g, c_per_group // 2, dim=1)
+            half_channels = self.channels_per_group // 2
+            x_g1, x_g2 = x_g[:, :half_channels, :], x_g[:, half_channels:, :]
             
             # Channel attention: global average pooling across spatial dimension
-            channel_avg = x_g1.mean(dim=2, keepdim=True)  # [batch_size, C//2G, 1]
+            channel_avg = x_g1.mean(dim=2, keepdim=True)  # [batch_size, half_channels, 1]
             channel_weights = self.channel_attention[g](channel_avg)
             x_g1_enhanced = x_g1 * channel_weights
             
             # Spatial attention: normalize and compute attention weights
-            x_g2_norm = self.group_norm(x_g2)
+            x_g2_norm = self.group_norms[g](x_g2)
             spatial_weights = self.spatial_attention[g](x_g2_norm.mean(dim=1, keepdim=True).transpose(1, 2))
             x_g2_enhanced = x_g2 * spatial_weights.transpose(1, 2)
             
@@ -81,7 +90,7 @@ class HybridAttentionModule(nn.Module):
         output = torch.cat(output_groups, dim=1).view(batch_size, C, L)
         
         # Channel shuffle operation for information exchange
-        output = output.view(batch_size, self.num_groups, C // self.num_groups, L)
+        output = output.view(batch_size, self.num_groups, self.channels_per_group, L)
         output = output.transpose(1, 2).contiguous().view(batch_size, C, L)
         
         return output
